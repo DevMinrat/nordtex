587f696b1ac41129e26c585243e7e3b125de1c13
Marat Minchik <devminrat@gmail.com>
2022-10-15 12:17:16 +0300

    added about page

--- src/assets/js/components/sliders.js
+++ src/assets/js/components/sliders.js
@@ -1,16 +1,17 @@
 if (document.querySelector(".main-news__slider")) {
   var weworksSlider = new Splide(".main-news__slider", {
     width: "100%",
-    perPage: 3,
+    // perPage: 3,
+    autoWidth: true,
     perMove: 1,
     gap: "4rem",
     padding: { left: "11rem", right: "10rem" },
     pagination: false,
     classes: {
-      arrows: 'splide__arrows your-class-arrows',
-      arrow : 'splide__arrow your-class-arrow',
-      prev  : 'splide__arrow--prev your-class-prev',
-      next  : 'splide__arrow--next your-class-next',
+      arrows: "splide__arrows your-class-arrows",
+      arrow: "splide__arrow your-class-arrow",
+      prev: "splide__arrow--prev your-class-prev",
+      next: "splide__arrow--next your-class-next",
     },
 
     // drag: "free",
@@ -19,5 +20,28 @@ if (document.querySelector(".main-news__slider")) {
 
     mediaQuery: "min",
     breakpoints: {},
+  }).mount();
+}
+
+if (document.querySelector(".about-gds__slider")) {
+  var weworksSlider = new Splide(".about-gds__slider", {
+    width: "100%",
+    // perPage: 3,
+    autoWidth: true,
+    perMove: 1,
+    gap: "4rem",
+    pagination: false,
+    arrows: false,
+
+    // drag: "free",
+    // snap: true,
+    // flickPower: 200,
+
+    mediaQuery: "min",
+    breakpoints: {
+      921: {
+        destroy: true,
+      },
+    },
   }).mount();
 }
