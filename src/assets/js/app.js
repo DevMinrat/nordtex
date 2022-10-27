dad8ef34e1317429348a9176ecb6a87b3b0a7f8b
Marat Minchik <devminrat@gmail.com>
2022-10-27 18:01:09 +0300

    added contacts page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -99,4 +99,147 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
+
+  /// required form fields
+  const forms = document.querySelectorAll("form");
+
+  if (forms.length > 0) {
+    forms.forEach((form) => {
+      const submitBtn = form.querySelector("button[type='submit']");
+
+      submitBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        setBlankFields(form);
+
+        if (checkReqFields(form)) {
+          form.submit();
+          form.querySelector(".error-req-fields").classList.add("hide");
+        }
+      });
+    });
+
+    function checkReqFields(form) {
+      const reqInputBoxes = form.querySelectorAll(".required-input__wrapper");
+      let flag = false;
+
+      reqInputBoxes.forEach((el) => {
+        const input = el.querySelector("input");
+
+        if (!input.validity.valid) {
+          el.classList.add("error");
+          flag = false;
+        } else {
+          el.classList.remove("error");
+          flag = true;
+        }
+      });
+
+      return flag;
+    }
+
+    function setBlankFields(form) {
+      const errReqField = form.querySelector(".error-req-fields");
+      const reqFieldsArea = errReqField.querySelector("#req-fields");
+      const reqInputs = form.querySelectorAll("input[required]");
+
+      let blankFieldsList = [];
+
+      reqInputs.forEach((input) => {
+        if (!input.validity.valid) {
+          blankFieldsList.push(input.dataset.name);
+
+          errReqField.classList.remove("hide");
+
+          reqFieldsArea.innerText = blankFieldsList.join(", ");
+        }
+      });
+    }
+  }
+
+  // tabs
+
+  class ItcTabs {
+    constructor(target, config) {
+      const defaultConfig = {};
+      this._config = Object.assign(defaultConfig, config);
+      this._elTabs =
+        typeof target === "string" ? document.querySelector(target) : target;
+      this._elButtons = this._elTabs.querySelectorAll(".tabs-btn");
+      this._elPanes = this._elTabs.querySelectorAll(".tabs-pane");
+      this._elPanesAddl = this._elTabs.querySelectorAll(".tabs-pane_addl");
+      this._eventShow = new Event("tab.itc.change");
+      this._init();
+      this._events();
+    }
+    _init() {
+      this._elTabs.setAttribute("role", "tablist");
+      this._elButtons.forEach((el, index) => {
+        el.dataset.index = index;
+        el.setAttribute("role", "tab");
+        this._elPanes[index].setAttribute("role", "tabpanel");
+        this._elPanesAddl[index].setAttribute("role", "tabpanel");
+      });
+    }
+    show(elLinkTarget) {
+      const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+      const elPaneAddlTarget = this._elPanesAddl[elLinkTarget.dataset.index];
+      const elLinkActive = this._elTabs.querySelector(".tabs-btn_active");
+      const elPaneShow = this._elTabs.querySelector(".tabs-pane_show");
+      const elPaneAddlShow = this._elTabs.querySelector(".tabs-pane_addl_show");
+      if (elLinkTarget === elLinkActive) {
+        return;
+      }
+      elLinkActive ? elLinkActive.classList.remove("tabs-btn_active") : null;
+      elPaneShow ? elPaneShow.classList.remove("tabs-pane_show") : null;
+      elPaneAddlShow ? elPaneAddlShow.classList.remove("tabs-pane_addl_show") : null;
+      elLinkTarget.classList.add("tabs-btn_active");
+      elPaneTarget.classList.add("tabs-pane_show");
+      elPaneAddlTarget.classList.add("tabs-pane_addl_show");
+      this._elTabs.dispatchEvent(this._eventShow);
+      elLinkTarget.focus();
+    }
+    showByIndex(index) {
+      const elLinkTarget = this._elButtons[index];
+      elLinkTarget ? this.show(elLinkTarget) : null;
+    }
+    _events() {
+      this._elTabs.addEventListener("click", (e) => {
+        const target = e.target.closest(".tabs-btn");
+        if (target) {
+          e.preventDefault();
+          this.show(target);
+        }
+      });
+    }
+  }
+
+  if (document.querySelector(".contacts-addrs")) {
+    new ItcTabs(".contacts-addrs");
+  }
+
+  // modal functioal
+
+  const modalTriggers = document.querySelectorAll("[data-modal]");
+
+  if (modalTriggers.length > 0) {
+    modalTriggers.forEach((el) => {
+      el.addEventListener("click", () => {
+        let modalName = el.dataset.modal;
+        let modal = document.querySelector(`[data-modalName='${modalName}']`);
+
+        modal.classList.remove("hide");
+      });
+    });
+  }
+
+  const modals = document.querySelectorAll(".modal");
+
+  if (modals.length > 0) {
+    modals.forEach((el) => {
+      el.querySelector("[data-close]").addEventListener("click", () => {
+        el.classList.add("hide");
+      });
+    });
+  }
 });
